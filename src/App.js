5f60b49e34e0bfd46cea2f03ab605006acebc6c3
Сучков Дмитрий Вадимович <Suchkov.D.Vad@sberbank.ru>
2019-11-17 15:03:15 +0300

    s

--- src/App.js
+++ src/App.js
@@ -1,15 +1,33 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import * as io from 'socket.io-client';
 import logo from './logo.svg';
 import './App.css';
-import Test from './Components/Test';
+import Main from './Components/Main';
+import Header from './Components/Header';
 
 function App() {
+  const socket = io('http://localhost:5000');
+  useEffect(() => {
+    // On connection
+    socket.on('connect', () => {
+      console.log('connected');
+
+});
+  // Connection failed
+  socket.on('error', function (err) {
+    console.log(err);
+    throw new Error(err);
+  });
+  socket.on('test', (data) =>{
+    console.log(data);
+})
+}, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Test />
-      </header>
+    <Header />
+    <Main socket={socket} />
+
     </div>
   );
 }
---
+++ src/Components/Header.jsx
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+const Header = () => {
+    return (
+      <div className="header">
+          <h1> logo </h1>
+        <div>
+        <h2>название цеха</h2>
+          <button>переключалка</button>
+      </div>
+      </div>
+    );
+}
+
+export default Header;
